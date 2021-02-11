b0515f55fd62d9055d5a1b3595edeb25ecc401be
Ala <66693414+AlaBahrami@users.noreply.github.com>
2021-02-11 00:11:33 -0600

    Update Streamflow_extarction.py

--- streamflow/Streamflow_extarction.py
+++ streamflow/Streamflow_extarction.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-The purpose of this script is to extarct streamflow records of the FRB.
+The purpose of this script is to extarct streamflow records for the Fraser .
 Then finding out which stations have missing data. Stations having missing records 
-are replaced with -1
+are replaced with -1. data less than 15% missing records are exported 
+
+!todo: 
+Data can be categorized into two series. Between 2004 to 
+2011 (calibration), and 2011 to 2017 (evaluation).  
 
 """
-
-
+# %% importing modules 
 import datetime
 import pandas as pd
-import numpy as np
+
+# %% setting parameters
+# input 
+sh = "Observations_flow_2004_2017" 
+filin = "Streamflow_FRB.xlsx"
+
+# output 
+filout   = "Streamflow_FRB_fill.xlsx"
+filout2  = "Streamflow_FRB_85%.xlsx"
+filindex = "stflo_index85.xlsx"
 
 
-#-----------------------------------------------------------------------------
-# start and end time of GEM_CaPA
-#ts = datetime.datetime(2004, 9 , 1, 00, 00, 00)
-#tf = datetime.datetime(2017 , 8 , 31 , 23 , 00 , 00);
-
+# %% start and end time of GEM_CaPA
 ts = datetime.datetime(2004, 9 , 1)
 tf = datetime.datetime(2017 , 8 , 31)
 
@@ -26,45 +32,27 @@ tf = datetime.datetime(2017 , 8 , 31)
 diff = tf - ts;
 time_rec = diff.total_seconds()/(3600*24) + 1 
 
-# if requried to transfer it to hours
-#time_rec = diff.total_seconds()/3600 +1
-
-#-----------------------------------------------------------------------------
-# reading streamflow records and replacing nan values with -1
-
-# setting parameters
-fil_in = "Observations_flow_2004_2017" 
-fil_out = "Streamflow_FRB_ed.xlsx"
-
-stflo = pd.read_excel(r'Streamflow_FRB.xlsx', sheet_name = fil_in) 
-stflo2 = stflo.drop(columns=['Unnamed: 0'])
+# %% reading streamflow records and filling missing values 
+sf  = pd.read_excel(filin, sh) 
+#sf2 = sf.drop(columns=['Unnamed: 0'])
 
 # finding null values 
-fid = stflo.isnull()
+fid = sf.isnull()
 
 # counting nan values 
-count_nan = len(stflo2) - stflo2.count()
-
-# convert to array
-#stflo.values 
-
-# extract to subarray
-# stflo.iloc[:,1]
+count_nan = len(sf) - sf.count()
 
 # replacing nan values 
-stflo_ed = stflo.fillna(value = -1) 
-stflo_ed.to_excel(fil_out)
+sfed = sf.fillna(value = -1) 
 
-#-----------------------------------------------------------------------------
-# filter out stations with full records or 75% valid records
+# %% filter out stations with full records or 85% valid records
+arful =  count_nan[count_nan == 0]
+ar85  =  count_nan[count_nan <= 0.15*(time_rec)]
 
-arr_ful =  count_nan[count_nan == 0]
-arr_90  =  count_nan[count_nan <= 0.1*(time_rec)]
+# %% extracting full records
+sfed2 = sfed[ar85.index] 
 
-arr_ful.to_excel("stflo_index.xlsx")
-
-
-#-----------------------------------------------------------------------------
-#extract with specific condition
-a = stflo_ed[stflo_ed == '-1']
-
+# %% write data (index and records)  
+sfed.to_excel(filout)
+sfed2.to_excel(filout2)
+ar85.to_excel(filindex)
