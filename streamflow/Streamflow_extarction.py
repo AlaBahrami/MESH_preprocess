ed84f06cc14abc0d06cf9725b5cabe2e195dfeae
Ala <66693414+AlaBahrami@users.noreply.github.com>
2021-02-17 18:21:18 -0600

    Add files via upload

--- streamflow/Streamflow_extarction.py
+++ streamflow/Streamflow_extarction.py
@@ -19,11 +19,12 @@ sh = "Observations_flow_2004_2017"
 filin = "Streamflow_FRB.xlsx"
 
 # output 
-filout     = "Streamflow_FRB_fill.xlsx"
-filout2    = "Streamflow_FRB_85%.xlsx"
-filindex   = "stflo_index85.xlsx"
-filmissing = "stflo_index_missing.xlsx"
-filcalib   = "Streamflow_FRB_calibration.xlsx"
+filout        = "Streamflow_FRB_fill.xlsx"
+filout2       = "Streamflow_FRB_85%.xlsx"
+filindex100   = "stflo_index100.xlsx"
+filindex85    = "stflo_index85.xlsx"
+filmissing    = "stflo_index_missing.xlsx"
+filcalib      = "Streamflow_FRB_calibration.xlsx"
 
 # %% start and end time of GEM_CaPA
 ts = datetime.datetime(2004, 9 , 1)
@@ -39,18 +40,12 @@ sf  = pd.read_excel(filin, sh)
 
 # finding null values 
 fid = sf.isnull()
-
 # counting nan values 
+
 count_nan = len(sf) - sf.count()
 
 # replacing nan values 
 sfed = sf.fillna(value = -1) 
-
-# %% finding indices of station for calibration 
-# here I set it manually
-#!+ find them automatically
-stations = ['08JC002','08KB001','08KH006','08MC018','08LF051','08MF040','08MF005']
-sfcal = sfed[stations] 
 
 # %% filter out stations with full records or 85% valid records
 arful =  count_nan[count_nan == 0]
@@ -59,9 +54,18 @@ ar85  =  count_nan[count_nan <= 0.15*(time_rec)]
 # %% extracting full records
 sfed2 = sfed[ar85.index] 
 
+# %% finding indices of station for calibration 
+# here I set it manually
+#!+ find them automatically
+stations = ['08JC002','08KB001','08KH006','08KG001','08LF051',
+            '08MB005','08MC018','08MF005','08MF040']
+sfcal = sfed[stations] 
+
+
 # %% write data (index and records)
 count_nan.to_excel(filmissing)
 sfed.to_excel(filout)
 sfed2.to_excel(filout2)
-ar85.to_excel(filindex)
+arful.to_excel(filindex100)
+ar85.to_excel(filindex85)
 sfcal.to_excel(filcalib)
